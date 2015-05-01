183765582cedf5f36078d3a7032b119b115276a9
fo40225 <fo40225@hotmail.com>
2015-05-01 14:13:10 +0800

    ECMA6 tested on Chrome 42 64bit

--- Stopwatch/Stopwatch/Stopwatch.ts
+++ Stopwatch/Stopwatch/Stopwatch.ts
@@ -1,1 +1,87 @@
-﻿ 
+﻿class Stopwatch {
+    private elapsed: number;
+    private startTimeStamp: number;
+    private isRunning: boolean;
+
+    public get IsRunning(): boolean {
+        return this.isRunning;
+    }
+
+    public static IsHighResolution: boolean;
+
+    public get ElapsedMilliseconds(): number {
+        return Math.round(this.GetRawElapsedMilliseconds() * 10000) / 10000;
+    }
+
+    //static constructor
+    static initialize() {
+        if (typeof performance !== "undefined" && typeof performance.now === "function") {
+            Stopwatch.IsHighResolution = true;
+        } else {
+            Stopwatch.IsHighResolution = false;
+        }
+    }
+
+    public constructor() {
+        this.Reset();
+    }
+
+    Start(): void {
+        if (!this.isRunning) {
+            this.startTimeStamp = this.GetTimestamp();
+            this.isRunning = true;
+        }
+    }
+
+    static StartNew(): Stopwatch {
+        var s: Stopwatch = new Stopwatch();
+        s.Start();
+        return s;
+    }
+
+    Stop(): void {
+        if (this.isRunning) {
+            var endTimeStamp: number = this.GetTimestamp();
+            var elapsedThisPeriod: number = endTimeStamp - this.startTimeStamp;
+            this.elapsed += elapsedThisPeriod;
+            this.isRunning = false;
+
+            if (this.elapsed < 0) {
+                this.elapsed = 0;
+            }
+        }
+    }
+
+    Reset(): void {
+        this.elapsed = 0;
+        this.isRunning = false;
+        this.startTimeStamp = 0;
+    }
+
+    Restart(): void {
+        this.elapsed = 0;
+        this.startTimeStamp = this.GetTimestamp();
+        this.isRunning = true;
+    }
+
+    private GetRawElapsedMilliseconds(): number {
+        var timeElapsed: number = this.elapsed;
+
+        if (this.isRunning) {
+            var currentTimeStamp: number = this.GetTimestamp();
+            var elapsedUntilNow: number = currentTimeStamp - this.startTimeStamp;
+            timeElapsed += elapsedUntilNow;
+        }
+        return timeElapsed;
+    }
+
+    private GetTimestamp(): number {
+        if (Stopwatch.IsHighResolution) {
+            return performance.now();
+        }
+        else {
+            return Date.now();
+        }
+    }
+}
+Stopwatch.initialize();
