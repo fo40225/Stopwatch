93ed4e76a9e483e749cf4ee56f1ad0a12713e43f
fo40225 <fo40225@hotmail.com>
2015-05-01 13:23:02 +0800

    Tested on ie6

--- Stopwatch/Stopwatch/Stopwatch.js
+++ Stopwatch/Stopwatch/Stopwatch.js
@@ -1,1 +1,70 @@
+var Stopwatch = (function () {
+    function Stopwatch() {
+        this.Reset();
+    }
+    Stopwatch.prototype.IsRunning = function () {
+        return this.isRunning;
+    };
+    Stopwatch.IsHighResolution = function () {
+        if (typeof performance !== "undefined" && typeof performance.now === "function") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Stopwatch.prototype.ElapsedMilliseconds = function () {
+        return Math.round(this.GetRawElapsedMilliseconds() * 10000) / 10000;
+    };
+    Stopwatch.prototype.Start = function () {
+        if (!this.isRunning) {
+            this.startTimeStamp = this.GetTimestamp();
+            this.isRunning = true;
+        }
+    };
+    Stopwatch.StartNew = function () {
+        var s = new Stopwatch();
+        s.Start();
+        return s;
+    };
+    Stopwatch.prototype.Stop = function () {
+        if (this.isRunning) {
+            var endTimeStamp = this.GetTimestamp();
+            var elapsedThisPeriod = endTimeStamp - this.startTimeStamp;
+            this.elapsed += elapsedThisPeriod;
+            this.isRunning = false;
+            if (this.elapsed < 0) {
+                this.elapsed = 0;
+            }
+        }
+    };
+    Stopwatch.prototype.Reset = function () {
+        this.elapsed = 0;
+        this.isRunning = false;
+        this.startTimeStamp = 0;
+    };
+    Stopwatch.prototype.Restart = function () {
+        this.elapsed = 0;
+        this.startTimeStamp = this.GetTimestamp();
+        this.isRunning = true;
+    };
+    Stopwatch.prototype.GetRawElapsedMilliseconds = function () {
+        var timeElapsed = this.elapsed;
+        if (this.isRunning) {
+            var currentTimeStamp = this.GetTimestamp();
+            var elapsedUntilNow = currentTimeStamp - this.startTimeStamp;
+            timeElapsed += elapsedUntilNow;
+        }
+        return timeElapsed;
+    };
+    Stopwatch.prototype.GetTimestamp = function () {
+        if (Stopwatch.IsHighResolution()) {
+            return performance.now();
+        }
+        else {
+            return (new Date()).getTime();
+        }
+    };
+    return Stopwatch;
+})();
 //# sourceMappingURL=Stopwatch.js.map
--- Stopwatch/Stopwatch/Stopwatch.ts
+++ Stopwatch/Stopwatch/Stopwatch.ts
@@ -1,1 +1,83 @@
-﻿ 
+﻿class Stopwatch {
+    private elapsed: number;
+    private startTimeStamp: number;
+    private isRunning: boolean;
+
+    public IsRunning(): boolean {
+        return this.isRunning;
+    }
+
+    public static IsHighResolution(): boolean {
+        if (typeof performance !== "undefined" && typeof performance.now === "function") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ElapsedMilliseconds(): number {
+        return Math.round(this.GetRawElapsedMilliseconds() * 10000) / 10000;
+    }
+
+    public constructor() {
+        this.Reset();
+    }
+
+    Start(): void {
+        if (!this.isRunning) {
+            this.startTimeStamp = this.GetTimestamp();
+            this.isRunning = true;
+        }
+    }
+
+    static StartNew(): Stopwatch {
+        var s: Stopwatch = new Stopwatch();
+        s.Start();
+        return s;
+    }
+
+    Stop(): void {
+        if (this.isRunning) {
+            var endTimeStamp: number = this.GetTimestamp();
+            var elapsedThisPeriod: number = endTimeStamp - this.startTimeStamp;
+            this.elapsed += elapsedThisPeriod;
+            this.isRunning = false;
+
+            if (this.elapsed < 0) {
+                this.elapsed = 0;
+            }
+        }
+    }
+
+    Reset(): void {
+        this.elapsed = 0;
+        this.isRunning = false;
+        this.startTimeStamp = 0;
+    }
+
+    Restart(): void {
+        this.elapsed = 0;
+        this.startTimeStamp = this.GetTimestamp();
+        this.isRunning = true;
+    }
+
+    private GetRawElapsedMilliseconds(): number {
+        var timeElapsed: number = this.elapsed;
+
+        if (this.isRunning) {
+            var currentTimeStamp: number = this.GetTimestamp();
+            var elapsedUntilNow: number = currentTimeStamp - this.startTimeStamp;
+            timeElapsed += elapsedUntilNow;
+        }
+        return timeElapsed;
+    }
+
+    private GetTimestamp(): number {
+        if (Stopwatch.IsHighResolution()) {
+            return performance.now();
+        }
+        else {
+            return (new Date()).getTime();
+        }
+    }
+}
